43f50d32f08562e4af25bbf08c14d0bf1db8e8d7
valentinbujdoso <valentinbujdoso@gmail.com>
2023-06-04 21:04:39 -0500

    Add D solution

--- W1D6-Associations-2/src/main/java/cs544/D/App.java
+++ W1D6-Associations-2/src/main/java/cs544/D/App.java
@@ -1,4 +1,43 @@
 package cs544.D;
 
+import cs544.C.Course;
+import cs544.C.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
 public class App {
+    private static EntityManagerFactory emf;
+    public static void main(String[] args) throws Exception {
+        emf = Persistence.createEntityManagerFactory("cs544");
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Customer customer1 = new Customer("Customer1");
+        Reservation reservation1 = new Reservation("01/01/2022");
+        Reservation reservation2 = new Reservation("02/01/2022");
+        customer1.addReservation(reservation1);
+        customer1.addReservation(reservation2);
+        em.persist(customer1);
+
+        em.getTransaction().commit();
+        em.close();
+
+
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Customer> query = em.createQuery("from Customer ", Customer.class);
+
+        List<Customer> customerList = query.getResultList();
+        for (Customer customer : customerList) {
+            System.out.println(customer);
+        }
+        em.getTransaction().commit();
+        em.close();
+    }
 }
--- W1D6-Associations-2/src/main/java/cs544/D/Customer.java
+++ W1D6-Associations-2/src/main/java/cs544/D/Customer.java
@@ -1,4 +1,28 @@
 package cs544.D;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservationList.add(reservation);
+    }
 }
